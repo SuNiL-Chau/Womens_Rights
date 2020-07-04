13164a488886315cc8dc0ceb2aaedd6257e002fd
Sunil Chaudhary <1234huts@gmail.com>
2020-07-04 18:01:29 +0530

    contact us functionality added i.e. php file

--- mail.php
+++ mail.php
@@ -21,27 +21,27 @@ mail($to,$email_subject,$email_body,$headers);
 header('Location: thank_you.html');
 
 
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
+// // Function to validate against any email injection attempts
+// function IsInjected($str)
+// {
+//   $injections = array('(\n+)',
+//               '(\r+)',
+//               '(\t+)',
+//               '(%0A+)',
+//               '(%0D+)',
+//               '(%08+)',
+//               '(%09+)'
+//               );
+//   $inject = join('|', $injections);
+//   $inject = "/$inject/i";
+//   if(preg_match($inject,$str))
+//     {
+//     return true;
+//   }
+//   else
+//     {
+//     return false;
+//   }
+// }
    
 ?> 
